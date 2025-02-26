edfb4261fce48a17311b2ebf45cb209b9f8d4d3f
DinhQuy <hdquy2003@gmail.com>
2025-02-26 14:44:23 +0700

    create layout signup activity

--- app/src/main/java/com/example/techstore/activity/SignUpActivity.java
+++ app/src/main/java/com/example/techstore/activity/SignUpActivity.java
@@ -1,6 +1,7 @@
 package com.example.techstore.activity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +10,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.techstore.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    LinearLayout signup_layout_login;
+    TextInputEditText signup_et_email, signup_et_password, signup_et_confirm_password, signup_et_phone;
+    MaterialButton signup_btn_signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
