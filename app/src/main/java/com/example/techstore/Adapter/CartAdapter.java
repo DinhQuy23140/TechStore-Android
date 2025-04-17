16974d01bd12c4779e6443996f971a445aed0dd3
DinhQuy <hdquy2003@gmail.com>
2025-04-17 16:02:04 +0700

    incre/decre quantity product, delete product in cart

--- app/src/main/java/com/example/techstore/Adapter/CartAdapter.java
+++ app/src/main/java/com/example/techstore/Adapter/CartAdapter.java
@@ -8,14 +8,15 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.techstore.R;
+import com.example.techstore.interfaces.OnClickProductInCart;
 import com.example.techstore.model.ProductInCart;
 
 import java.math.BigDecimal;
@@ -26,10 +27,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder> {
     Context context;
     List<ProductInCart> listProductInCart;
+    OnClickProductInCart onClickProductInCart;
 
-    public CartAdapter(Context context, List<ProductInCart> listProductInCart) {
+    public CartAdapter(Context context, List<ProductInCart> listProductInCart, OnClickProductInCart onClickProductInCart) {
         this.context = context;
         this.listProductInCart = listProductInCart;
+        this.onClickProductInCart = onClickProductInCart;
     }
 
     @NonNull
@@ -39,7 +42,7 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
         return new CartViewHolder(view);
     }
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint({"SetTextI18n", "NotifyDataSetChanged"})
     @Override
     public void onBindViewHolder(@NonNull CartViewHolder holder, int position) {
         ProductInCart productInCart = listProductInCart.get(position);
@@ -48,31 +51,44 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
                 .load(image)
                 .into(holder.ivImg);
         holder.tvTitle.setText(productInCart.getTitle());
-        holder.tvSize.setText(productInCart.getSize());
-        AtomicInteger quantity = new AtomicInteger(productInCart.getQuantity());
+        holder.tvSize.setText("Size = " + productInCart.getSize());
         holder.tvQuantity.setText(Integer.toString(productInCart.getQuantity()));
-        float pricePro = quantity.get() * productInCart.getPrice();
+        float pricePro = productInCart.getQuantity() * productInCart.getPrice();
         DecimalFormat decimalFormat = new DecimalFormat("0.00");
         holder.tvPrice.setText(decimalFormat.format(pricePro) + "$");
         holder.cvColor.setCardBackgroundColor(productInCart.getColor());
         holder.btnDecrease.setOnClickListener(decrease -> {
-            quantity.getAndIncrement();
-            BigDecimal quantityProduct = new BigDecimal(quantity.get());
-            BigDecimal price = new BigDecimal(Float.toString(productInCart.getPrice()));
-            BigDecimal total = quantityProduct.multiply(price);
-            holder.tvPrice.setText(decimalFormat.format(total) + "$");
-            holder.tvQuantity.setText(Integer.toString(quantity.get()));
-        });
-
-        holder.btnIncrease.setOnClickListener(increase -> {
-            if (quantity.get() > 0) {
-                quantity.getAndDecrement();
-                BigDecimal quantityProduct = new BigDecimal(quantity.get());
+            int quantity = productInCart.getQuantity();
+            if (quantity > 0) {
+                quantity --;
+                BigDecimal quantityProduct = new BigDecimal(quantity);
                 BigDecimal price = new BigDecimal(Float.toString(productInCart.getPrice()));
                 BigDecimal total = quantityProduct.multiply(price);
                 holder.tvPrice.setText(decimalFormat.format(total) + "$");
-                holder.tvQuantity.setText(Integer.toString(quantity.get()));
+                holder.tvQuantity.setText(Integer.toString(quantity));
+                productInCart.setQuantity(quantity);
+                Toast.makeText(context, Integer.toString(quantity), Toast.LENGTH_SHORT).show();
+                onClickProductInCart.onDecreaseProductInCart(productInCart);
             }
+        });
+
+        holder.btnIncrease.setOnClickListener(increase -> {
+            int quantity = productInCart.getQuantity();
+            quantity ++;
+            BigDecimal quantityProduct = new BigDecimal(quantity);
+            BigDecimal price = new BigDecimal(Float.toString(productInCart.getPrice()));
+            BigDecimal total = quantityProduct.multiply(price);
+            holder.tvPrice.setText(decimalFormat.format(total) + "$");
+            holder.tvQuantity.setText(Integer.toString(quantity));
+            productInCart.setQuantity(quantity);
+            Toast.makeText(context, Integer.toString(quantity), Toast.LENGTH_SHORT).show();
+            onClickProductInCart.onIncreaseProductInCart(productInCart);
+        });
+
+        holder.ivDelete.setOnClickListener(delete -> {
+            onClickProductInCart.onDeteleProductInCart(productInCart);
+            listProductInCart.remove(productInCart);
+            notifyDataSetChanged();
         });
     }
 
