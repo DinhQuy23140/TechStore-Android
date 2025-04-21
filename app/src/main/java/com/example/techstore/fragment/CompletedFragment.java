9833c5d67a89a97918df6daec37e2c0927deae26
DinhQuy <hdquy2003@gmail.com>
2025-04-21 09:21:51 +0700

    order-fragment-layout

--- app/src/main/java/com/example/techstore/fragment/CompletedFragment.java
+++ app/src/main/java/com/example/techstore/fragment/CompletedFragment.java
@@ -5,25 +5,24 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.techstore.Adapter.ViewPagerAdapter;
 import com.example.techstore.R;
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
 
-import java.util.Objects;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link OrdersFragment#newInstance} factory method to
+ * Use the {@link CompletedFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class OrdersFragment extends Fragment {
+public class CompletedFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,8 +32,9 @@ public class OrdersFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private RecyclerView recyclerView;
 
-    public OrdersFragment() {
+    public CompletedFragment() {
         // Required empty public constructor
     }
 
@@ -44,11 +44,11 @@ public class OrdersFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment OrdersFragment.
+     * @return A new instance of fragment CompletedFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static OrdersFragment newInstance(String param1, String param2) {
-        OrdersFragment fragment = new OrdersFragment();
+    public static CompletedFragment newInstance(String param1, String param2) {
+        CompletedFragment fragment = new CompletedFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -69,23 +69,11 @@ public class OrdersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_orders, container, false);
+        return inflater.inflate(R.layout.fragment_completed, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TabLayout tabLayout = view.findViewById(R.id.tabLayout);
-        ViewPager2 viewPager = view.findViewById(R.id.viewPager);
-
-        ViewPagerAdapter adapter = new ViewPagerAdapter(requireActivity());
-        viewPager.setAdapter(adapter);
-
-        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
-            if (position == 0)
-                tab.setText("Ongoing");
-            else
-                tab.setText("Completed");
-        }).attach();
     }
 }
