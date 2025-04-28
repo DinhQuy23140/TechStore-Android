91ea4a60a7a280370200bddd1d55f327fa08a4bb
DinhQuy <hdquy2003@gmail.com>
2025-04-28 22:12:38 +0700

    create orderstatus, layout-item-status-orders, adapter-status-orders

---
+++ app/src/main/java/com/example/techstore/Adapter/OrderStatusAdapter.java
@@ -0,0 +1,51 @@
+package com.example.techstore.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.techstore.R;
+import com.example.techstore.model.OrderStatus;
+
+import java.util.List;
+
+public class OrderStatusAdapter extends RecyclerView.Adapter<OrderStatusAdapter.OrderStatusViewHolder> {
+    Context context;
+    List<OrderStatus> orderStatuses;
+
+    @NonNull
+    @Override
+    public OrderStatusViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_order_status, parent, false);
+        return new OrderStatusViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OrderStatusViewHolder holder, int position) {
+        OrderStatus status = orderStatuses.get(position);
+        holder.tvStatusName.setText(status.getStatus());
+        holder.tvStatusTime.setText(status.getTimestamp());
+    }
+
+    @Override
+    public int getItemCount() {
+        return orderStatuses.size();
+    }
+
+    public class OrderStatusViewHolder extends RecyclerView.ViewHolder {
+        TextView tvStatusName, tvStatusTime;
+        ImageView imgStatusIcon;
+        public OrderStatusViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvStatusName = itemView.findViewById(R.id.tv_status_name);
+            tvStatusTime = itemView.findViewById(R.id.tv_status_time);
+            imgStatusIcon = itemView.findViewById(R.id.img_status_icon);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/techstore/model/OrderStatus.java
@@ -0,0 +1,26 @@
+package com.example.techstore.model;
+
+public class OrderStatus {
+    String status, timestamp;
+
+    public OrderStatus(String status, String timestamp) {
+        this.status = status;
+        this.timestamp = timestamp;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
